3f42f4bf8f317c1e75a144753afb9e431b8468e4
Leon Li <qianthinking@gmail.com>
2025-08-17 12:28:47 +0800

    feat(zsh): add new zsh aliases
    Introduces several new zsh aliases to enhance command-line productivity.
    
    These aliases simplify common operations such as:
    - Navigating parent directories (`...`, `....`, `.....`)
    - Filtering and processing command output (`C`, `H`, `L`, `N`, `S`, `G`, `GV`, `A`)
    - Extracting fields from output (`P1`, `P2`, `P3`)
    - Managing processes (`K`, `K9`)

--- chezmoi/dot_zsh.after/030-aliases.zsh
+++ chezmoi/dot_zsh.after/030-aliases.zsh
@@ -26,6 +26,24 @@ alias chd='cd ~/Downloads'
 alias chw='cd ~/workspace'
 alias cho='cd ~/opensource'
 
+# Ohter Aliases
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g C='| wc -l'
+alias -g H='| head'
+alias -g L="| less"
+alias -g N="| /dev/null"
+alias -g S='| sort'
+alias -g G='| grep' # now you can do: ls foo G something
+alias -g GV='| grep -v grep'
+alias -g A='| awk'
+alias -g P1='"{print \$1}"'
+alias -g P2='"{print \$2}"'
+alias -g P3='"{print \$3}"'
+alias -g K='| xargs kill'
+alias -g K9='| xargs kill -9'
+
 # function to re-add a directory for chezmoi with forget and add
 function chreadd() {
   if [[ -d "$1" ]]; then
